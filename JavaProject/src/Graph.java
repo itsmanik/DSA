a87a80f5be2599e41f34cf2fb567496a5ff8bf05
itsmanik <manik.hilalpure@gmail.com>
2024-07-14 18:56:04 +0530

    changed graph values

--- JavaProject/src/Graph.java
+++ JavaProject/src/Graph.java
@@ -5,11 +5,11 @@ public class Graph {
     static class Edge {
         int src;
         int dest;
-        int wt;
-        public Edge(int s, int d, int w) {
+//        int wt;
+        public Edge(int s, int d) {
             this.src = s;
             this.dest = d;
-            this.wt = w;
+//            this.wt = w;
         }
     }
 
@@ -17,28 +17,36 @@ public class Graph {
         for(int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<Edge>();
         }
-        graph[0].add(new Edge(0, 2, 2));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
 
-        graph[1].add(new Edge(1, 2, 10));
-        graph[1].add(new Edge(1, 3, 0));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
 
-        graph[2].add(new Edge(2, 0, 2));
-        graph[2].add(new Edge(2, 1, 10));
-        graph[2].add(new Edge(2, 3, -1));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
 
-        graph[3].add(new Edge(3, 1, -1));
-        graph[3].add(new Edge(3, 2, 0));
+        graph[3].add(new Edge(3, 1));
+        graph[2].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
     }
-
     public static void main(String[] args) {
-        int v = 4;
+        int v = 7;
         ArrayList<Edge> graph[] = new ArrayList[v];
         createGraph(graph);
-
         // print 3's neighbour
-        for(int i = 0; i < graph[2].size(); i++) {
-            Edge e = graph[2].get(i);
-            System.out.println(e.dest + " " + e.wt);
-        }
+//        for(int i = 0; i < graph[2].size(); i++) {
+//            Edge e = graph[2].get(i);
+//            System.out.println(e.dest + " ");
+//        }
     }
 }
